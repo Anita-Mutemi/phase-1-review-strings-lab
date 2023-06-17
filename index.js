c1e444e16f6bb0d4e396ed29b94a7f6bbf6b6e79
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-17 22:41:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Step 1: Define the currentUser
+const currentUser = 'Grace Hopper';
+
+// Step 2: Define the welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Step 3: Define the excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Step 4: Define the shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
